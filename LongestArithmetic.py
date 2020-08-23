e5b17001c60aee992b6ece735dee28d18cf4989b
SHUBHAM SINGH <36537624+singh2407shubham@users.noreply.github.com>
2020-08-23 13:24:14 +0530

    Create LongestArithmetic.py

---
+++ LongestArithmetic.py
@@ -0,0 +1,33 @@
+def lengthOflongestArray(array, array_len):
+    
+    
+    longest = 0
+    stack = []
+    
+    if array_len < 2: return
+    
+    for Idx in range(1, array_len):
+        diff = array[Idx] - array[Idx-1]
+        if not stack:
+            stack.append(diff)
+            currLongest = 2
+            
+        else:
+            if diff == stack[-1]:
+                currLongest += 1
+            else:
+                stack.pop()
+                currLongest = 2
+                stack.append(diff)
+                
+        longest = max(longest, currLongest)
+        
+    return longest
+
+    
+t = int(input())
+for i in range(1, t+1):
+    length = int(input())
+    test_array = [int(item) for item in input().split()]
+    ans = lengthOflongestArray(test_array, length)
+    print("Case #{}: {}". format(i, ans))
